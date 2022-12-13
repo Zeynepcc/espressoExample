92bcf90c5a460b92da669593c39bb392da37a44d
Zeynep Turhan <zeynepturhan0@gmail.com>
2022-12-14 01:12:51 +0300

    komutlar2
    devamı..

--- LoginExample/app/src/androidTest/java/com/example/loginexample/ExampleInstrumentedTest.java
+++ LoginExample/app/src/androidTest/java/com/example/loginexample/ExampleInstrumentedTest.java
@@ -1,23 +1,31 @@
 package com.example.loginexample;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.content.Context;
+import android.view.View;
 
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.hamcrest.CoreMatchers.allOf;
 import static org.junit.Assert.*;
 
 /**
@@ -31,6 +39,17 @@ public class ExampleInstrumentedTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule =
             new ActivityScenarioRule<>(MainActivity.class);
+    private View decorView ;
+    @Before
+    public void setUp()
+    {
+        activityRule.getScenario().onActivity(new ActivityScenario.ActivityAction<MainActivity>() {
+            @Override
+            public void perform(MainActivity activity) {
+                decorView = activity.getWindow().getDecorView();
+            }
+        });
+    }
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -51,22 +70,28 @@ public class ExampleInstrumentedTest {
     @Test
     public void placeholderControl()
     {
-
+        onView(withId(R.id.mail)).check(matches(withHint("Mail")));
     }
     @Test
     public void buttonClickControl()
     {
-
+        //onView(withId(R.id.signinbtn)).perform(click());
+        onView(withText("LOGIN")).perform(click());
     }
     @Test
-    public  void falseLoginControl()
-    {
+    public  void toastMessageControl() throws InterruptedException {
 
+        onView(withId(R.id.mail))
+                .perform(typeText("root"));
+        onView(withId(R.id.password)).perform(typeText("12345"));
+        onView(ViewMatchers.isRoot()).perform(ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.signinbtn)).perform(click());
+        //Thread.sleep(2000);
+        onView(withText("Giriş Yapıldı"))
+                .inRoot(withDecorView(Matchers.not(decorView)))// Here you use decorView
+                .check(matches(isDisplayed()));
+        //onView(allOf(withText(containsString("Giriş Yapıldı")))).check(matches(isDisplayed()));
     }
-    @Test
-    public void trueLoginControl()
-    {
 
-    }
 
 }
